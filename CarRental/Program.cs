f91741d9ff7af9f9ebbe7a31ac95d79034050caa
Felipe  Romao <feliperomao.a@gmail.com>
2022-04-05 08:36:16 -0300

    Add project files.

---
+++ CarRental/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using CarRental.Entities;
+using CarRental.Services;
+
+namespace CarRental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Rental carRental = new Rental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine();
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
